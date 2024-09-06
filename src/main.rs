29912104c3379c5253f2ed19590372da2499bfe2
David <dmcfarland8031@gmail.com>
2024-09-05 18:55:49 -0600

    Refactored mode line and left margin into their own structs. Consolidated several functions/methods into single draw function. Began adding tests and documentation.

--- src/main.rs
+++ src/main.rs
@@ -4,275 +4,294 @@
 extern crate libc;
 
 use mr_text::ffi::*;
-use std::{borrow::BorrowMut, io::{BufRead, Read, Write}};
+use std::{
+    borrow::BorrowMut,
+    io::{BufRead, Read, Write},
+};
 
 fn main() {
     let mut screen = Screen::new()
-        .populate_winsize()
-        .build_left_edge()
-        .build_bottom_line();
+        .init_left_margin()
+        .init_mode_line()
+        .init_text_window()
+        .backup_terminal();
 
-    let original_term = match ffi::tc_getattr(&mut screen.terminal.istream) {
-        Ok(backup) => backup,
-        Err(err) => panic!("Error: {}", err),
-    };
+    screen.clear();
+    screen.position_cursor("1", "3");
+    Screen::draw(&mut screen.io.ostream, &screen.mode_line.seperator_line);
+    Screen::draw(&mut screen.io.ostream, &screen.left_margin.seperator_line);
 
-    screen.clear_screen();
-    screen.render_edges();
-    screen.position_cursor(1, 3);
-    screen.render_mode_line();
-    ffi::configure_raw(&mut screen.terminal.istream).unwrap();
-
-    screen.update_point();
-//    println!("{:?}{:?}", screen.point.t_c, screen.point.t_r);
+    ffi::configure_raw(&mut screen.io.istream).unwrap();
 
     loop {
-        screen
-            .terminal
-            .read_buffer()
-            .expect("Failed to read from stdin.");
-        let mut start = 0;
-        let mut end = screen.terminal.buffer.len();
-        let to_consume = screen.terminal.buffer.len();
-
-        screen.terminal.validate_utf8(start, end);
-        match screen.terminal.buffer.into_iter().next().unwrap() {
-            // 8 is backspace
-            2 => screen.keybinding_movement('D', None), // ctrl + b
-            4 => {}
-            6 => screen.keybinding_movement('C', None), // ctrl + f
-            9 => screen.terminal.write_input(start, to_consume), // tab
-            10 => {
-                screen.terminal.cr_nl(to_consume);
-            } // carriage return new line
-            14 => screen.keybinding_movement('B', None), // ctrl + p
-            16 => screen.keybinding_movement('A', None), // ctrl + n
-            17 => {
-                // Clear istream and quit.
-                screen.terminal.istream.lock().consume(to_consume);
-                break;
-            }
-            32..=126 => {
-                screen.terminal.write_input(start, to_consume);
-            }
-            // 127 is del
-            _ => {
-                screen.terminal.istream.lock().consume(to_consume);
-            }
-        }
-        screen.update_point();
-        screen.render_mode_line();
+        break;
     }
-    screen.clear_screen();
-    let _revert_on_drop = ffi::RevertOnDrop::new(screen.terminal.istream.by_ref(), original_term);
-}
-#[derive(Debug)]
-pub struct Screen {
-    terminal: Terminal,
-    point: Point,
-    left_edge: String,
-    bottom_line: String,
-    mode_line: String,
-    winsize_row: u16,
-    winsize_col: u16,
+    screen.clear();
+    let _revert_on_drop = ffi::RevertOnDrop::new(screen.io.istream.by_ref(), screen.io.original_term);
 }
 
-impl Screen {
-    pub fn new() -> Self {
-        Screen {
-            terminal: Terminal::new(),
-            point: Point::new(),
-            left_edge: String::new(),
-            bottom_line: String::new(),
-            mode_line: " ".to_string(),
-            winsize_row: 0,
-            winsize_col: 0,
-        }
-    }
+pub struct Screen<'a> {
+    io: IO,
+    point: Point<'a>,
+    text_window: TextWindow,
+    mode_line: ModeLine,
+    tab_line: TabLine,
+    left_margin: LeftMargin,
+}
 
-    pub fn populate_winsize(mut self) -> Screen {
+pub trait Builder<'a> {
+    fn init_text_window(self) -> Screen<'a>;
+    fn init_mode_line(self) -> Screen<'a>;
+    fn new_ml_sep_line(&mut self);
+    fn init_left_margin(self) -> Screen<'a>;
+    fn backup_terminal(self) -> Screen<'a>;
+}
+
+impl<'a> Builder<'a> for Screen<'a> {
+    /// init_text_window has to be built last so that the thickness of the lines
+    /// and margins have been populated.
+    ///
+    /// Saftey concern. Possible uninizialized memory in init_text_window().
+    /// Panic on error.
+
+    fn init_text_window(mut self) -> Screen<'a> {
         let mut istream = std::io::stdin();
-        // Saftey concern. Possible uninizialized memory? Terminate on error.
         let winsize = match ffi::io_ctl(istream.by_ref()) {
             Ok(winsize) => winsize,
-            Err(err) => panic!("{}", err),
+            Err(err) => panic!("Possible uninitialized memory. \nError: {}", err),
         };
-        self.winsize_row = winsize.ws_row;
-        self.winsize_col = winsize.ws_col;
+        self.text_window = TextWindow {
+            winsize_row: winsize.ws_row,
+            winsize_col: winsize.ws_col,
+            text_area_row: winsize.ws_row - self.mode_line.thickness,
+            text_area_col: winsize.ws_col - self.left_margin.thickness,
+        };
         self
     }
 
-    /// This builder method must be called after populate_winsize().
-    pub fn build_left_edge(mut self) -> Screen {
-        let winsize_row = self.winsize_row - 2;
-        for _ in 0..winsize_row {
-            self.left_edge.push_str("~\n\r");
+    fn init_left_margin(mut self) -> Screen<'a> {
+        self.left_margin.new_sep('~');
+        self.left_margin.new_thickness(2);
+
+        let sep_str = format!("{}\n\r", self.left_margin.seperator);
+        for _ in 0..self.text_window.winsize_row {
+            self.left_margin.seporator_line.push_str(&sep_str);
         }
         self
     }
 
-    /// This builder method must be called after populate_winsize().
-    pub fn build_bottom_line(mut self) -> Screen {
-        for _ in 0..self.winsize_col {
-            self.bottom_line.push_str("=");
+    fn init_mode_line(mut self) -> Screen<'a> {
+        self.mode_line.new_sep('=');
+        self.mode_line.new_thickness(3);
+
+        let mut bite_buf = [0; 4];
+        let sep_as_str = self.mode_line.seperator.encode_utf8(&mut bite_buf);
+        for _ in 0..self.text_window.winsize_col {
+            self.mode_line.seperator_line.push_str(sep_as_str);
         }
         self
     }
 
-    pub fn get_point_pos(&mut self) {
-        let mut lock = self.terminal.istream.lock();
-
-        write!(self.terminal.ostream, "\x1b[6n").expect("Write failed in get_point_pos().");
-        self.terminal.ostream.flush().expect("Failed to flush.");
-        let output = lock.fill_buf().unwrap();
-        let len = output.len();
-        self.point.escape_buf = output.iter().map(|val| *val).collect::<Vec<u8>>();
-        lock.consume(len);
+    fn new_ml_sep_line(&mut self) {
+        let mut bite_buf = [0; 4];
+        let sep_as_str = self.mode_line.seperator.encode_utf8(&mut bite_buf);
+        // Only reallocates if winsize.winsize_col has increased.
+        self.mode_line.seperator_line.clear();
+        for _ in 0..self.text_window.winsize_col {
+            self.mode_line.seperator_line.push_str(sep_as_str);
+        }
+        // TODO: Add draw method here when I have one written.
     }
 
-    pub fn update_point(&mut self) {
-        self.get_point_pos();
-        // trim the R
-        let len = self.point.escape_buf.len() - 1;
-        let escape_slice = &self.point.escape_buf[2..len];
-        let semicolon_idx = escape_slice.iter().position(|&val| val == 59).unwrap();
-        let row_col = escape_slice.split_at(semicolon_idx);
-        let col = row_col.1.strip_prefix(&[59]).unwrap();
-        self.point.r = row_col.0.iter().map(|val| *val - b'0').collect::<Vec<u8>>();
-        self.point.row = (col[0] as u16) << 8;
-        self.point.c = col.iter().map(|val| *val - b'0').collect::<Vec<u8>>();
+    fn backup_terminal(mut self) -> Screen<'a> {
+        self.io.original_term = match ffi::tc_getattr(&mut self.io.istream) {
+            Ok(backup) => Some(backup),
+            Err(err) => panic!("Error: {}", err),
+        };
+        self
     }
+}
 
-    pub fn position_cursor(&mut self, row: u16, col: u16) {
+impl<'a> Screen<'a> {
+    pub fn new() -> Screen<'a> {
+        Screen {
+            io: IO::new(),
+            point: Point::default(),
+            text_window: TextWindow::default(),
+            mode_line: ModeLine::default(),
+            tab_line: TabLine::default(),
+            left_margin: LeftMargin::default(),
+        }
+    }
+    pub fn position_cursor(&mut self, row: &'a str, col: &'a str) {
         self.point.row = row;
         self.point.col = col;
-        write!(self.terminal.ostream, "\x1b[{};{}H", row, col).unwrap();
-        self.terminal.ostream.flush().unwrap();
+        write!(self.io.ostream, "\x1b[{};{}H", row, col).unwrap();
+        self.io.ostream.flush().unwrap();
     }
 
-    pub fn render_mode_line(&mut self) {
-        let pos = (self.point.row, self.point.col);
-        let mode_text = format!("R: {}, C: {}", self.point.row, self.point.col);
-        self.position_cursor(self.winsize_col, 0);
-        write!(self.terminal.ostream, "{}", mode_text).expect("Failed to write mode line.");
-        self.terminal.ostream.flush().unwrap();
-        self.position_cursor(pos.0.into(), pos.1.into());
+    pub fn clear(&mut self) {
+        write!(self.io.ostream, "{}[2J", 27 as char).expect("Write failed in clear_screen().");
+        self.io.ostream.flush().expect("Failed post write flush.");
     }
 
-    pub fn clear_screen(&mut self) {
-        write!(self.terminal.ostream, "{}[2J", 27 as char)
-            .expect("Write failed in clear_screen().");
-        self.terminal
+    //         .write_all("\r\n~ ".as_bytes())
+
+    pub fn draw<T>(stream: &mut T, text: &str)
+    where
+        T: Write + ?Sized,
+    {
+        stream
+            .write_all(text.as_bytes())
+            .expect("Failed to write buffer to ostream.");
+        stream.flush().unwrap();
+    }
+
+    pub fn write_key_stroke(&mut self) {
+        let start = self.io.buffer.len() - self.io.buf_len;
+        self.io
             .ostream
-            .flush()
-            .expect("Failed post write flush.");
+            .write_all(&self.io.buffer[start..])
+            .expect("Failed to write buffer to ostream.");
+        self.io.ostream.flush().expect("Failed post write flush.");
+        self.io.istream.lock().consume(self.io.buf_len);
     }
 
-    pub fn keybinding_movement(&mut self, dir: char, lines: Option<u8>) {
-        if lines.is_some() {
-            write!(self.terminal.ostream, "\x1b[{}{}", lines.unwrap(), dir)
-                .expect("Write failed in move_cursor().");
-            self.terminal
-                .ostream
-                .flush()
-                .expect("Failed post write flush.")
-        } else {
-            write!(self.terminal.ostream, "\x1b[1{}", dir).expect("Write failed in move_cursor().");
-            self.terminal
-                .ostream
-                .flush()
-                .expect("Failed post write flush.");
+    pub fn rebuild_ml_seperator_line(&mut self) {
+        let mut bite_buf = [0; 4];
+        let sep_as_str = self.mode_line.seperator.encode_utf8(&mut bite_buf);
+        for _ in 0..self.text_window.winsize_col {
+            self.mode_line.seperator_line.push_str(sep_as_str);
         }
-        self.terminal.ostream.flush().unwrap();
-        self.terminal.istream.lock().consume(1);
-    }
-
-    pub fn render_edges(&mut self) {
-        write!(self.terminal.ostream, "{}", self.left_edge).unwrap();
-        write!(self.terminal.ostream, "{}", self.bottom_line).unwrap();
-        write!(self.terminal.ostream, "{}", self.mode_line).unwrap();
-        self.terminal.ostream.flush().unwrap();
     }
 }
 
 #[derive(Debug)]
-pub struct Terminal {
+pub struct IO {
     istream: std::io::Stdin,
     ostream: std::io::Stdout,
-    buffer: [u8; 1],
+    estream: std::io::Stderr,
+    buffer: Vec<u8>,
+    buf_len: usize,
+    original_term: Option<libc::termios>,
 }
-
-impl Terminal {
-    pub fn new() -> Self {
-        Terminal {
+impl IO {
+    pub fn new() -> IO {
+        IO {
             istream: std::io::stdin(),
             ostream: std::io::stdout(),
-            buffer: [0; 1],
+            estream: std::io::stderr(),
+            buffer: Vec::with_capacity(512), // temp data struct
+            buf_len: 0,
+            original_term: None,
         }
     }
 
-    pub fn write_input(&mut self, start: usize, end: usize) {
-        self.ostream
-            .write_all(&self.buffer[start..end])
-            .expect("Failed to write buffer to ostream.");
-        self.ostream.flush().expect("Failed post write flush.");
-        self.istream.lock().consume(end);
+    pub fn store_buffer<T: std::iter::IntoIterator<Item = u8> + ExactSizeIterator>(
+        &mut self,
+        buf: T,
+    ) {
+        self.buf_len = buf.len();
+        self.buffer = buf.into_iter().collect::<Vec<_>>()
     }
 
-    pub fn read_buffer(&mut self) -> std::io::Result<()> {
-        match self.istream.lock().fill_buf() {
-            Ok(buf) => {
-                self.buffer.copy_from_slice(&buf);
-                Ok(())
-            }
-            Err(error) => Err(error),
-        }
+    pub fn write_esc_seq(&mut self, escape_seq: &str) {
+        write!(self.ostream, "{}", escape_seq).expect("Failed to write escape seq.");
+        self.ostream.flush().unwrap();
+    }
+    pub fn read_esc_resp(&mut self) {
+        todo!()
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Point<'a> {
+    row: &'a str,
+    col: &'a str,
+}
+impl<'a> Point<'a> {
+    pub fn update_pos(&mut self, row: &'a str, col: &'a str) {
+        self.row = row;
+        self.col = col;
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct TextWindow {
+    winsize_row: u16,
+    winsize_col: u16,
+    text_area_row: u16,
+    text_area_col: u16,
+}
+impl TextWindow {}
+
+/// Constructs a new mode line struct. The initial field value of ModeLine
+/// thickness is 3 and the initial seperator is '='. Example shows how to change these settings.
+///
+/// Use Screen.new_ml_sep_line() to refresh the seporator_line field and apply changes.
+/// # Examples
+///
+/// ```
+///
+/// let mut mode_line = ModeLine::default();
+///
+/// mode_line.new_set('a');
+/// mode_line.new_thickness(10);
+///
+/// assert_eq!(mode_line.seperator, 'a');
+/// assert_eq!(left_margin.thickness, 10);
+/// ``
+
+#[derive(Debug, Default, Clone)]
+pub struct ModeLine {
+    thickness: u8,
+    seperator: char,
+    seperator_line: String,
+}
+
+impl ModeLine {
+    pub fn new_sep(&mut self, sep: char) {
+        self.seperator = sep
     }
 
-    pub fn cr_nl(&mut self, end: usize) {
-        self.ostream
-            .write_all("\r\n~ ".as_bytes())
-            .expect("Failed to write buffer to ostream.");
-        self.ostream
-            .flush()
-            .expect("Failed to unwrap post write flush.");
-        self.istream.lock().consume(end);
+    pub fn new_thickness(&mut self, thickness: u8) {
+        self.thickness = thickness
+    }
+}
+
+/// Constructs a new left margin struct. The initial field value of LeftMargin
+/// thickness is 2 and the initial seperator is '~'. Example shows how to change these settings.
+///
+/// # Examples
+///
+/// ```
+///
+/// let mut left_margin = LeftMargin::default();
+///
+/// left_margin.new_set('a');
+/// left_margin.new_thickness(10);
+///
+/// assert_eq!(left_margin.seperator, 'a');
+/// assert_eq!(left_margin.thickness, 10);
+/// ``
+
+#[derive(Debug, Default, Clone)]
+pub struct LeftMargin {
+    thickness: u8,
+    seperator: char,
+    seperator_line: String,
+}
+
+impl LeftMargin {
+    fn new_sep(&mut self, sep: char) {
+        self.seperator = sep
     }
 
-    pub fn validate_utf8(&mut self, mut start: usize, mut end: usize) {
-        while let Err(err) = std::str::from_utf8(&self.buffer[start..end]) {
-            if let Some(invalid) = err.error_len() {
-                let valid = start + err.valid_up_to();
-                write!(self.ostream, "{}", char::REPLACEMENT_CHARACTER)
-                    .expect("Failed to write buffer to ostream.");
-                start = valid + invalid;
-                self.ostream.flush().expect("Failed post write flush.");
-                self.istream.lock().consume(end);
-            } else {
-                // This could be a valid char whose UTF-8 byte sequence is spanning multiple chunks.
-                end = start + err.valid_up_to();
-            }
-        }
+    fn new_thickness(&mut self, thickness: u8) {
+        self.thickness = thickness
     }
 }
 
 #[derive(Debug, Default, Clone)]
-pub struct Point {
-    row: u16,
-    col: u16,
-    r: Vec<u8>,
-    c: Vec<u8>,
-    escape_buf: Vec<u8>, 
-}
-
-impl Point {
-    fn new() -> Self {
-        Point {
-            row: 0,
-            col: 0,
-            r: Vec::with_capacity(5),
-            c: Vec::with_capacity(5),
-            escape_buf: Vec::with_capacity(14),
-        }
-    }
-}
+pub struct TabLine {}
+impl TabLine {}
